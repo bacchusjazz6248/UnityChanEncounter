406428b3f523af20af9de26f6951189c04b33560
bacchusjazz <bacchusjazz6248@gmail.com>
2020-01-22 07:07:51 +0900

    敵追跡スクリプト追加・クエリちゃん追加(おまけ)

---
+++ UnityChanEncounter/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public GameObject target;
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
