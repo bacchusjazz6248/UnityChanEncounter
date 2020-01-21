6f90653f4b36e345caddec8ce1650913520c16cc
bacchusjazz <bacchusjazz6248@gmail.com>
2020-01-22 01:57:02 +0900

    キャラクターステータスファイル(武器・アイテム無し)設定

---
+++ UnityChanEncounter/Assets/Scripts/Status/AllyStatus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+[CreateAssetMenu(fileName = "AllyStatus", menuName = "CreateAllyStatus")]
+public class AllyStatus : CharacterStatus
+{
+
+    //　獲得経験値
+    [SerializeField]
+    private int earnedExperience = 0;
+
+    public void SetEarnedExperience(int earnedExperience)
+    {
+        this.earnedExperience = earnedExperience;
+    }
+
+    public int GetEarnedExperience()
+    {
+        return earnedExperience;
+    }
+}
