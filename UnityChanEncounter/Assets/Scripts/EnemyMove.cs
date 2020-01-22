805380a14ace335a44c3ef0a294fc5244153c75f
bacchusjazz <bacchusjazz6248@gmail.com>
2020-01-22 09:23:47 +0900

    敵追跡スクリプト強化

--- UnityChanEncounter/Assets/Scripts/EnemyMove.cs
+++ UnityChanEncounter/Assets/Scripts/EnemyMove.cs
@@ -1,22 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMove : MonoBehaviour
 {
+    private NavMeshAgent _agent;
 
-    public GameObject target;
-    NavMeshAgent agent;
-    // Use this for initialization
-    void Start()
+    private void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        _agent = GetComponent<NavMeshAgent>(); // NavMeshAgentを保持しておく
     }
 
-    // Update is called once per frame
-    void Update()
+    // CollisionDetectorのonTriggerStayにセットし、衝突判定を受け取るメソッド
+    public void OnDetectObject(Collider collider)
     {
-        agent.destination = target.transform.position;
+        // 検知したオブジェクトに「Player」のタグがついていれば、そのオブジェクトを追いかける
+        if (collider.CompareTag("Player"))
+        {
+            _agent.destination = collider.transform.position;
+        }
     }
 }
